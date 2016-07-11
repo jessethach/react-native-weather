249e7bf4d79b62141585c85c3e70c6b045d227c0
jessethach <jessethach@gmail.com>
2016-07-11 09:06:50 -0700

    initial commit

---
+++ src/api.js
@@ -0,0 +1,20 @@
+import _ from 'lodash';
+const API_KEY = 'eb11b309431a1cc4994633299d0b95da';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`;
+
+var kelvinToF = function(kelvin) {
+  return Math.round((kelvin - 273.15) * 1.8 + 32) + ' °F';
+}
+
+export default function(latitude, longitude) {
+  var url = `${ROOT_URL}&lat=${latitude}&lon=${longitude}`;
+  return fetch(url)
+    .then((response) => response.json())
+    .then((responseData) => {
+      return {
+        city: responseData.name,
+        temperature: kelvinToF(responseData.main.temp),
+        description: _.capitalize(responseData.weather[0].description)
+      }
+    });
+}
